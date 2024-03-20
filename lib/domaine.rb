186f6b123f7727e27ca8c2d585e273aac0412f0d
Mark Kadlec <mkadlec@plansource.com>
2024-03-20 10:56:19 -0600

    Refactor and add comments

--- lib/domaine.rb
+++ lib/domaine.rb
@@ -1,11 +1,20 @@
 module Domain
+
+  VALID_DKIM_KEY = 'ANSWER: 1'.freeze
+
+  ##################################################################
+  # This method is used to validate the domain and selector and return
+  # true if the domain and selector are listed in the DNS records.
+  ##################################################################
   def self.valid_dkim(domain, selector)
     dig_command = "dig txt #{selector}._domainkey.#{domain}"
     valid_domain = false
+
     IO.popen(dig_command) do |stdout|
       output = stdout.read
-      valid_domain = "output: #{output.include? 'ANSWER: 1'}" if output
+      valid_domain = output.include?(VALID_DKIM_KEY) if output
     end
+
     valid_domain
   end
 end
